6d691e91422acc6475da49cd1095d8783426f827
Sachin Kumar Patel <sachinkumarpatel1996@gmail.com>
2018-04-28 05:03:11 +0530

    deleting

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports={
-    consumer_key:         'Ef7FxSIve7PlbCR2gjSe38oHR',
-    consumer_secret:      'S8h9PgHYaTGrIbhJbZ9XV5Z64fNtdmm957Y0qyEwr5n9vI21pf',
-    access_token:         '989958706300440576-ROFUui3nRqVRVp8LWOkNhCqeXhLhAuI',
-    access_token_secret:  'WAowu8XwwcSYulCklxgVyM7x0hn1QjbxrzeQ9gt4HmvOh',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-};
