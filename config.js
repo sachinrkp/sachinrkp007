fe3166d34eadcb82f7fa2c591f115b131c4897d3
Sachin Kumar Patel <sachinkumarpatel1996@gmail.com>
2018-04-28 05:07:52 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports={
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_SECRET
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+};
